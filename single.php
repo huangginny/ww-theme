56550990cda1f1870ea2b9eda5eba28afe6a1a5f
Ginny Huang <jh92710@gmail.com>
2020-04-26 23:13:43 -0400

    navigation

--- single.php
+++ single.php
@@ -19,7 +19,13 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			the_post_navigation( array(
+				'screen_reader_text' => __(""),
+				'prev_text' => __("<< 上一篇：%title"),
+				'next_text' => __("下一篇：%title >>"),
+				'in_same_term' => true,
+				'taxonomy' => __( 'post_tag' ),
+			));
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
